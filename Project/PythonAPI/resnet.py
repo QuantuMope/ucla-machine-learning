d6bc76bc7b3778d1cd0e5bcb17d0ed77885b8b32
cheimu <xerhoneyfc@gmail.com>
2020-06-06 15:40:18 -0700

    load/store img embeding into csv file

--- Project/PythonAPI/resnet.py
+++ Project/PythonAPI/resnet.py
@@ -19,7 +19,6 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import pylab
 import pickle
-
 def initLinear(linear, val = None):
     if val is None:
         fan = linear.in_features +  linear.out_features 
@@ -28,7 +27,6 @@ def initLinear(linear, val = None):
         spread = val
         linear.weight.data.uniform_(-spread,spread)
         linear.bias.data.uniform_(-spread,spread)
-
 class resnet_34(nn.Module):
     def __init__(self):
         super(resnet_34, self).__init__()
@@ -39,43 +37,53 @@ class resnet_34(nn.Module):
         self.dropout = nn.Dropout(.5)
         self.relu = nn.LeakyReLU()
         initLinear(self.linear)
-
     def base_size(self): return 512
     def rep_size(self): return 1024
-
     def forward(self, x):
+        x = x.float()
         x = self.resnet.conv1(x)
         x = self.resnet.bn1(x)
         x = self.resnet.relu(x)
         x = self.resnet.maxpool(x)
-
         x = self.resnet.layer1(x)
         x = self.resnet.layer2(x)
         x = self.resnet.layer3(x)
         x = self.resnet.layer4(x)
-
         x = self.dropout2d(x)
-
         return self.dropout(self.relu(self.linear(x.view(-1, 7*7*self.base_size()))))
-
-
+def load_batch(dataset, batch_sz):
+    n_batches = (len(dataset) + batch_sz - 1) // batch_sz
+    for i in range(0, n_batches):
+        yield i, dataset[i * batch_sz : (i + 1) * batch_sz]
 if __name__ == "__main__":
-
-    model = resnet_34()
-
+    if torch.cuda.is_available():
+        device = "cuda:0"
+    else:
+        device = "cpu"
+    FEATURE_DATA = "./feature_embedding.csv"
     IMG_DATA_DIRECTORY = './preprocessed_testing_img_data.pkl'
+    TEST_DATA_DIRECTORY = "./preprocessed_testing_img_data_extremely_small.pkl"
     OUTPUT_DIRECTORY = './feature_embedding.csv'
-    all_test_img_data = np.load(IMG_DATA_DIRECTORY)
+    # features = np.load(FEATURE_DATA)
+    # print(features.shape)
+    # print(features)
+    # exit(0)
+    model = resnet_34().to(device)
+    all_test_img_data = np.load(TEST_DATA_DIRECTORY)
+    print(all_test_img_data.shape)
     # with open(IMG_DATA_DIRECTORY, 'rb') as f:
     #     all_test_img_data = pickle.load(f)
     img_data = torch.from_numpy(all_test_img_data)
-    count = 0
-    for img in img_data:
+    for i, img in load_batch(img_data, 10):
         #img = torch.from_numpy(img_data)
-        print("begin to generate embedding")
-        rep = model(img)
-        print("saving...")
-        with open(OUTPUT_DIRECTORY, 'wb') as f:
-            pickle.dump(rep, f, protocol=pickle.HIGHEST_PROTOCOL)
-            pickle.dump(", ", f, protocol=pickle.HIGHEST_PROTOCOL)
-        print(count+1)
+        # print("begin to generate embedding")
+        # img = img.unsqueeze(0).to(device)
+        img = img.to(device)
+        rep = model(img).detach().numpy()
+        print("[{}] saving {}".format(i, img.shape))
+        #with open(OUTPUT_DIRECTORY, 'ab') as f:
+        #    pickle.dump(rep, f, protocol=pickle.HIGHEST_PROTOCOL)
+        np.savetxt(FEATURE_DATA, rep, delimiter=', ')
+
+    restored_testing_img = np.genfromtxt(FEATURE_DATA, delimiter=',', dtype=float)
+    print(restored_testing_img)
