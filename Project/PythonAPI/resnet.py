d2310f5adae19d59a36c72b49b730c401da6d004
cheimu <xerhoneyfc@gmail.com>
2020-06-05 21:05:25 -0700

    upload resnet34

---
+++ Project/PythonAPI/resnet.py
@@ -0,0 +1,77 @@
+import os
+import time
+import argparse
+from torch import optim
+import random as rand
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.utils.data as data
+import torchvision as tv
+import torchvision.transforms as tvt
+import numpy as np
+import math
+import json
+import sys
+from pycocotools.coco import COCO
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+import pickle
+
+def initLinear(linear, val = None):
+    if val is None:
+        fan = linear.in_features +  linear.out_features 
+        spread = math.sqrt(2.0) * math.sqrt( 2.0 / fan )
+    else:
+        spread = val
+        linear.weight.data.uniform_(-spread,spread)
+        linear.bias.data.uniform_(-spread,spread)
+
+class resnet_34(nn.Module):
+    def __init__(self):
+        super(resnet_34, self).__init__()
+        self.resnet = tv.models.resnet34(pretrained=True)
+        # probably want linear, relu, dropout
+        self.linear = nn.Linear(7*7*512, 1024)
+        self.dropout2d = nn.Dropout2d(.5)
+        self.dropout = nn.Dropout(.5)
+        self.relu = nn.LeakyReLU()
+        initLinear(self.linear)
+
+    def base_size(self): return 512
+    def rep_size(self): return 1024
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        x = self.resnet.layer4(x)
+
+        x = self.dropout2d(x)
+
+        return self.dropout(self.relu(self.linear(x.view(-1, 7*7*self.base_size()))))
+
+
+if __name__ == "__main__":
+
+    IMG_DATA_DIRECTORY = './testing_img_data'
+
+
+    model = resnet_34()
+    
+    with open(IMG_DATA_DIRECTORY, 'rb') as f:
+        all_test_img_data = pickle.load(f)
+
+    count = 0
+    for image in all_test_img_data:
+        img = torch.from_numpy(image)
+        rep = model(img)
+        torch.save(rep, '/Users/cheimu/ucla-machine-learning/project/PythonAPI/feature_embedding')
+        print(count+1)
